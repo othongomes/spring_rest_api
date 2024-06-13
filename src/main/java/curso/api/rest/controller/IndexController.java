4061c7d71e0dc78aadf2db44da078ca066ad55a6
User <othonmarcello@hotmail.com>
2024-06-13 15:19:12 -0300

    Curso Spring Rest API

---
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -0,0 +1,99 @@
+package curso.api.rest.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+
+@RestController /* Arquitetura REST */
+@RequestMapping(value = "/usuario")
+public class IndexController {
+
+	@Autowired /* de fosse CDI seria @Inject */
+	private UsuarioRepository usuarioRepository;
+
+	/* Serviço RESTful */
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
+
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete(@PathVariable("id") Long id) {
+
+		usuarioRepository.deleteById(id);
+
+		return "ok";
+	}
+
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuario() {
+
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody @Valid Usuario usuario) {
+
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+
+		String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhacriptografada);
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+
+	}
+
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+
+		/* outras rotinas antes de atualizar */
+
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+
+		Usuario userTemporario = usuarioRepository.findUserByLogin(usuario.getLogin());
+
+		if (!userTemporario.getSenha().equals(usuario.getSenha())) { /* Senhas diferentes */
+			String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+			usuario.setSenha(senhacriptografada);
+		}
+
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+
+	}
+
+	/*
+	 * @GetMapping(value = "/othon", produces = "application/json") public
+	 * ResponseEntity inicio() { return new ResponseEntity("Othon", HttpStatus.OK);
+	 * }
+	 */
+
+}
